f5c00940217eb7ded51191c13ab6fdc18e64d60c
darView <g.scholtes@darview.com>
2019-03-10 20:39:25 +0100

    Add forgotten vacancies pages.

--- page-text-round-image.php
+++ page-text-round-image.php
@@ -4,29 +4,54 @@
 */
 $about_image        = get_field('about_image');
 $about_story        = get_field('about_story');
-
+$vacatures_found    = false;
 get_header();
+$loop = new WP_Query(['post_type' => 'actuele_vacatures', 'orderby' => 'post_id', 'order' => 'ASC']);
+while ($loop->have_posts()) {
+    $loop->the_post();
+    if (!get_field('vacature_gesloten')) {
+        $vacatures_found = true;
+        break;
+    }
+}
+$loop->rewind_posts();
 ?>
-<section id="text-round-image">
 
-    <div class="container">
+    <section id="text-round-image">
+
+        <div class="container">
 
 
-        <h1 class="js-wp js-wp-title h1 my-5 text-center"><?php single_post_title(); ?></h1>
+            <h1 class="js-wp js-wp-title h1 my-5 text-center"><?php single_post_title(); ?></h1>
 
-        <div class="row mb-5">
+            <div class="row mb-5">
 
-            <div class="js-wp js-wp-about-1 col col-lg-3 col-md-4 col-sm-6 text-center mb-5">
-                <img class="image" src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>"/>
+                <div class="js-wp js-wp-about-1 col col-lg-3 col-md-4 col-sm-6 text-center mb-5">
+                    <img class="image" src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>"/>
+                    <?php if (single_post_title("", false) == 'Vacatures'): ?>
+                        <?php if ($vacatures_found): ?>
+                            <div class="current-vacancies text-left">
+                                <div class="head">Actuele vacatures</div>
+                                <ul class="list-unstyled">
+                                    <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                                        <?php if (!get_field('vacature_gesloten')): ?>
+                                            <?php $tooltip = get_field('vacature_parallax_heading') . ": " . lcfirst(get_field('vacature_functie')) . " te " . get_field('vacature_locatie'); ?>
+                                            <li><a href="<?php echo the_permalink();?>" data-toggle="tooltip" data-placement="top" title="<?php echo $tooltip ?>"><small>&gt;</small> <?php the_field('vacature_functie'); ?></a></li>
+                                        <?php endif; ?>
+                                    <?php endwhile; ?>
+                                </ul>
+                            </div>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                </div>
+
+                <div class="js-wp js-wp-about-2 col col-lg-9 col-md-8 col-sm-6">
+                    <?php echo ($about_story); ?>
+                </div>
+
             </div>
-
-            <div class="js-wp js-wp-about-2 col col-lg-9 col-md-8 col-sm-6">
-                <?php echo ($about_story); ?>
-            </div>
-
         </div>
-    </div>
-</section>
+    </section>
 
 <?php
 get_footer();
---
+++ single-actuele_vacatures.php
@@ -0,0 +1,74 @@
+<?php
+/*
+    Template Name: Dienst Page
+*/
+$vacature_intro             = get_field('vacature_intro');
+$vacature_body              = get_field('vacature_body');
+$vacature_image             = get_field('vacature_image');
+$vacature_parallax_heading  = get_field('vacature_parallax_heading');
+$vacature_parallax_lead     = get_field('vacature_functie');
+$vacature_lokaal            = get_field('vacature_lokaal');
+$vacature_deeltijd          = get_field('vacature_deeltijd');
+$vacature_gesloten          = get_field('vacature_gesloten');
+if ($vacature_gesloten) {
+    $vacature_intro = '';
+    $vacature_body = '';
+    $vacature_parallax_heading = 'Helaas, deze vacature is inmiddels gesloten.';
+    $vacature_parallax_lead = '';
+    $vacature_deeltijd = false;
+    $vacature_lokaal = false;
+}
+get_header();
+?>
+    <div id="vacature">
+        <div class="container mb-5">
+
+            <?php
+            if ( function_exists('yoast_breadcrumb') ) {
+                yoast_breadcrumb(
+                    '<p id="breadcrumbs">',
+                    '</p>'
+                );
+            }
+            ?>
+            <h1 class="js-wp js-wp-title h1 my-5 text-center"><?php echo the_title(); ?></h1>
+            <p><?php echo $vacature_intro; ?></p>
+        </div>
+
+        <div class="parallax-window" data-parallax="scroll" data-image-src="<?php echo $vacature_image['url']; ?>" data-ios-fix="true" data-android-fix="true">
+            <div class="row">
+                <div class="col">
+                    <div class="container py-sm-5 py-4">
+                        <?php if (!empty($vacature_parallax_heading)) : ?>
+                            <h1 class="js-wp js-wp-home-1"><?php echo $vacature_parallax_heading; ?></h1>
+                        <?php endif; ?>
+                        <?php if (!empty($vacature_parallax_lead)) : ?>
+                            <p class="js-wp js-wp-home-1 d-none d-sm-block pb-5"><?php echo $vacature_parallax_lead; ?></p>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container my-5">
+            <?php if (!empty($vacature_body)) : ?>
+                <p><?php echo $vacature_body; ?></p>
+            <?php endif; ?>
+            <?php if ($vacature_lokaal): ?>
+                <p>Woon je in de (directe omgeving van) <?php the_field('vacature_locatie'); ?>, dan is dat een pre.</p>
+            <?php endif; ?>
+            <?php if ($vacature_deeltijd): ?>
+                <p>Deze vacature kan door meerdere personen ingevuld worden op de verschillende dagen. Dus ook al ben je
+                    niet op alle dagen beschikbaar, neem dan toch gerust contact met ons op.</p>
+            <?php endif; ?>
+            <?php if (!$vacature_gesloten): ?>
+                <p>Spreekt deze vacature je aan? Stuur je CV met motivatie en pasfoto naar
+                    <a title="" href="mailto:hr@herfstzorg.nl" data-toggle="tooltip" data-placement="top" data-original-title="E-mailadres">hr@herfstzorg.nl</a>
+                    of neem telefonisch contact met ons op via 06 24 127 539 onder vermelding van ‘<strong>V<?php echo get_the_ID() ?></strong>’.</p>
+            <?php endif; ?>
+        </div>
+    </div>
+
+
+<?php
+get_footer();
